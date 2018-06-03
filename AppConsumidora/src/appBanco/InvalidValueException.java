a60b6a9c3a045744ee549e0e9664308bee2d172e
Artur RSO <artur.rso@outlook.com>
2018-06-03 18:54:30 -0300

    Arquivos do projeto
    Classes dos formulários, arquivos de configuração e arquivos gerados ao consumir o web service.

---
+++ AppConsumidora/src/appBanco/InvalidValueException.java
@@ -0,0 +1,13 @@
+package appBanco;
+
+/**
+ *
+ * @author ArturRSO
+ */
+public class InvalidValueException extends Exception {
+    
+    public InvalidValueException(){}
+    
+    public InvalidValueException(String message){}
+    
+}
